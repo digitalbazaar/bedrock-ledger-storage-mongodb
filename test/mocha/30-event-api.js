8bb841b798d66dd9dc9ce3bb824ef9e988136f23
Manu Sporny <msporny@digitalbazaar.com>
2017-06-01 10:09:02 -0400

    Add event API test framework.

--- test/mocha/30-event-api.js
+++ test/mocha/30-event-api.js
@@ -36,7 +36,50 @@ describe('Event Storage API', () => {
         done(err);
       });
     });
-    it('does X', done => {
+    it.skip('should create event', done => {
+      done();
+    });
+    it.skip('should get event', done => {
+      done();
+    });
+    it.skip('should update event', done => {
+      done();
+    });
+    it.skip('should delete event', done => {
+      done();
+    });
+    it.skip('should not create event in non-owned ledger', done => {
+      done();
+    });
+    it.skip('should not get event from non-owned ledger', done => {
+      done();
+    });
+    it.skip('should not update event in non-owned ledger', done => {
+      done();
+    });
+    it.skip('should not delete event in non-owned ledger', done => {
+      done();
+    });
+  });
+  describe('admin as actor', () => {
+    const mockIdentity = mockData.identities.regularUser;
+    let actor;
+    before(done => {
+      brIdentity.get(null, mockIdentity.identity.id, (err, result) => {
+        actor = result;
+        done(err);
+      });
+    });
+    it.skip('should create event in any ledger', done => {
+      done();
+    });
+    it.skip('should get event from any ledger', done => {
+      done();
+    });
+    it.skip('should update event in any ledger', done => {
+      done();
+    });
+    it.skip('should delete event in any ledger', done => {
       done();
     });
   });
