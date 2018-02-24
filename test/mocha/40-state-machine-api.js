b9f9dfed74b0a05d2032b5c51d68e7aa7778fd0e
Dave Longley <dlongley@digitalbazaar.com>
2018-02-23 19:37:35 -0500

    Fix state machine query bugs and add index for sorting.

--- test/mocha/40-state-machine-api.js
+++ test/mocha/40-state-machine-api.js
@@ -60,7 +60,7 @@ describe('State Machine Storage API', () => {
     // FIXME: Remove ledger
     done();
   });
-  it('should get state machine object by id', done => {
+  it.only('should get state machine object by id', done => {
     const blockTemplate = eventBlockTemplate;
     const eventTemplate = mockData.events.alpha;
     let operation1;
@@ -98,6 +98,90 @@ describe('State Machine Storage API', () => {
         // recordOne.meta.blockHeight.should.equal(1);
         recordOne.object.should.deep.equal(operation1.record);
         const recordTwo = results.getTwo;
+        recordTwo.object.should.deep.equal(operation2.record);
+        recordOne.meta.blockHeight.should.equal(2);
+        callback();
+      }]
+    }, err => {
+      assertNoError(err);
+      done(err);
+    });
+  });
+  it.only('should get two state machine objects from different blocks by id',
+    done => {
+    const blockTemplate = eventBlockTemplate;
+    const eventTemplate = mockData.events.alpha;
+    let operation1;
+    let operation2;
+    async.auto({
+      create: callback => helpers.createBlocks(
+        {blockTemplate, eventTemplate, blockNum: 2}, (err, result) => {
+          operation1 = result.events[0].event.operation[0];
+          operation2 = result.events[1].event.operation[0];
+          callback(err, result);
+        }),
+      event: ['create', (results, callback) => {
+        async.each(results.create.events, (e, callback) =>
+          ledgerStorage.events.add(
+            {event: e.event, meta: e.meta}, callback), callback);
+      }],
+      block: ['event', (results, callback) => {
+        async.each(results.create.blocks, ({block, meta}, callback) =>
+          ledgerStorage.blocks.add({block, meta}, callback), callback);
+      }],
+      get: ['block', 'event', (results, callback) => {
+        ledgerStorage.stateMachine.get(operation1.record.id, callback);
+      }],
+      getTwo: ['get', (results, callback) => {
+        ledgerStorage.stateMachine.get(operation2.record.id, callback);
+      }],
+      ensureObject: ['get', 'getTwo', (results, callback) => {
+        const recordOne = results.get;
+        should.exist(recordOne);
+        should.exist(recordOne.object);
+        should.exist(recordOne.object.id);
+        should.exist(recordOne.meta);
+        should.exist(recordOne.meta.blockHeight);
+        // FIXME: should blockHeight equal the block that contains the event?
+        // recordOne.meta.blockHeight.should.equal(1);
+        recordOne.object.should.deep.equal(operation1.record);
+        const recordTwo = results.getTwo;
+        recordTwo.object.should.deep.equal(operation2.record);
+        recordOne.meta.blockHeight.should.equal(2);
+        callback();
+      }],
+      create2: ['ensureObject', (results, callback) => helpers.createBlocks(
+        {blockTemplate, eventTemplate, blockNum: 2}, (err, result) => {
+          operation1 = result.events[0].event.operation[0];
+          operation2 = result.events[1].event.operation[0];
+          callback(err, result);
+        })],
+      event2: ['create2', (results, callback) => {
+        async.each(results.create2.events, (e, callback) =>
+          ledgerStorage.events.add(
+            {event: e.event, meta: e.meta}, callback), callback);
+      }],
+      block2: ['event2', (results, callback) => {
+        async.each(results.create2.blocks, ({block, meta}, callback) =>
+          ledgerStorage.blocks.add({block, meta}, callback), callback);
+      }],
+      get2: ['block2', 'event2', (results, callback) => {
+        ledgerStorage.stateMachine.get(operation1.record.id, callback);
+      }],
+      getTwo2: ['get2', (results, callback) => {
+        ledgerStorage.stateMachine.get(operation2.record.id, callback);
+      }],
+      ensureObject2: ['get2', 'getTwo2', (results, callback) => {
+        const recordOne = results.get2;
+        should.exist(recordOne);
+        should.exist(recordOne.object);
+        should.exist(recordOne.object.id);
+        should.exist(recordOne.meta);
+        should.exist(recordOne.meta.blockHeight);
+        // FIXME: should blockHeight equal the block that contains the event?
+        // recordOne.meta.blockHeight.should.equal(1);
+        recordOne.object.should.deep.equal(operation1.record);
+        const recordTwo = results.getTwo2;
         recordTwo.object.should.deep.equal(operation2.record);
         recordOne.meta.blockHeight.should.equal(2);
         callback();
