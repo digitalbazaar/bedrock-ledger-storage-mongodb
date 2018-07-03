8dbfe7b086b63f88c071351497c2c7048a9be456
Matthew Collier <collier.matthew@gmail.com>
2018-07-03 18:16:15 -0400

    Implement extension of storage APIs.

--- test/mocha/40-plugin.js
+++ test/mocha/40-plugin.js
@@ -6,10 +6,9 @@
 const async = require('async');
 const bedrock = require('bedrock');
 const blsMongodb = require('bedrock-ledger-storage-mongodb');
-// const database = require('bedrock-mongodb');
-// const {expect} = global.chai;
 const helpers = require('./helpers');
 const mockData = require('./mock.data');
+const mockPlugin = require('./mock.plugin');
 const uuid = require('uuid/v4');
 
 const exampleLedgerId = `did:v1:${uuid()}`;
@@ -20,50 +19,161 @@ configEventTemplate.ledger = exampleLedgerId;
 const configBlockTemplate = bedrock.util.clone(mockData.configBlocks.alpha);
 configBlockTemplate.id = exampleLedgerId + '/blocks/1';
 
-// mock plugin
-blsMongodb.use('foo', {
-  type: 'service',
-  api: {
-    index: async ({createIndexes, collections}) => {
-      should.exist(createIndexes);
-      createIndexes.should.be.a('function');
-      should.exist(collections);
-      collections.should.an('object');
-      should.exist(collections.blockCollection);
-      collections.blockCollection.should.be.a('string');
-      should.exist(collections.eventCollection);
-      collections.eventCollection.should.be.a('string');
-      should.exist(collections.operationCollection);
-      collections.operationCollection.should.be.a('string');
-      await createIndexes([{
-        collection: collections.operationCollection,
-        fields: {'operation.record.type': 1},
-        options: {unique: false, background: false, name: 'fooIndex'}
-      }]);
-    }
-  }
-});
+// register mock plugin
+blsMongodb.use('foo', mockPlugin);
 
 describe('Storage Plugin API', () => {
-  it('plugin adds an index to the operations collection', done => {
-    const meta = {};
-    const options = {
-      ledgerId: exampleLedgerId, ledgerNodeId: exampleLedgerNodeId,
-      services: ['foo']
-    };
-    async.auto({
-      storage: callback => blsMongodb.add(meta, options, callback),
-      test: ['storage', (results, callback) => {
-        const {storage} = results;
-        storage.operations.collection.indexInformation({}, (err, result) => {
-          assertNoError(err);
-          should.exist(result.fooIndex);
-          callback();
-        });
-      }]
-    }, err => {
-      assertNoError(err);
-      done();
+  describe('index API', () => {
+    it('plugin adds an index to the operations collection', done => {
+      const meta = {};
+      const options = {
+        ledgerId: exampleLedgerId, ledgerNodeId: exampleLedgerNodeId,
+        services: ['foo']
+      };
+      async.auto({
+        storage: callback => blsMongodb.add(meta, options, callback),
+        test: ['storage', (results, callback) => {
+          const {storage} = results;
+          storage.operations.collection.indexInformation({}, (err, result) => {
+            assertNoError(err);
+            should.exist(result.fooIndex);
+            callback();
+          });
+        }]
+      }, err => {
+        assertNoError(err);
+        done();
+      });
     });
   });
+  describe('mock record query API', () => {
+    let ledgerStorage;
+
+    beforeEach(done => {
+      const block = bedrock.util.clone(configBlockTemplate);
+      const meta = {};
+      const options = {
+        ledgerId: exampleLedgerId, ledgerNodeId: exampleLedgerNodeId,
+        services: ['foo']
+      };
+
+      async.auto({
+        initStorage: callback => blsMongodb.add(
+          meta, options, (err, storage) => {
+            ledgerStorage = storage;
+            callback(err, storage);
+          }),
+        eventHash: callback => helpers.testHasher(
+          configEventTemplate, callback),
+        blockHash: callback => helpers.testHasher(block, callback),
+        addEvent: ['initStorage', 'eventHash', (results, callback) => {
+          const meta = {
+            blockHeight: 0,
+            blockOrder: 0,
+            consensus: true,
+            consensusDate: Date.now(),
+            eventHash: results.eventHash
+          };
+          ledgerStorage.events.add(
+            {event: configEventTemplate, meta}, callback);
+        }],
+        addConfigBlock: ['addEvent', 'blockHash', (results, callback) => {
+          // blockHash and consensus are normally created by consensus plugin
+          meta.blockHash = results.blockHash;
+          meta.consensus = Date.now();
+          block.blockHeight = 0;
+          block.event = [results.eventHash];
+          ledgerStorage.blocks.add({block, meta}, callback);
+        }]
+      }, done);
+    });
+    it('queries for operations by type', done => {
+      async.auto({
+        concerts: callback => {
+          const eventTemplate = mockData.events.alpha;
+          const opTemplate = mockData.operations.alpha;
+          helpers.addEvent({
+            consensus: true, count: 5, eventTemplate, ledgerStorage, opTemplate
+          }, callback);
+        },
+        offers: ['concerts', (results, callback) => {
+          const eventTemplate = mockData.events.alpha;
+          const opTemplate = mockData.operations.gamma;
+          const eventHashes = Object.keys(results.concerts);
+          opTemplate.record.event = results.concerts[eventHashes[0]]
+            .operations[0].operation.record.id;
+          helpers.addEvent({
+            consensus: true, count: 1, eventTemplate, ledgerStorage, opTemplate
+          }, callback);
+        }],
+        recordAlpha: ['offers', (results, callback) => {
+          const eventHashes = Object.keys(results.concerts);
+          const concertIds = [
+            results.concerts[eventHashes[0]].operations[0].operation.record.id,
+            results.concerts[eventHashes[1]].operations[0].operation.record.id,
+          ];
+          ledgerStorage.operations.query({
+            maxBlockHeight: 100,
+            query: {
+              type: 'Offer',
+              event: concertIds,
+            }
+          }, (err, result) => {
+            assertNoError(err);
+            should.exist(result);
+            result.should.be.an('object');
+            should.exist(result.records);
+            result.records.should.be.an('array');
+            result.records.should.have.length(1);
+            const offerEventHashes = Object.keys(results.offers);
+            result.records[0].should.equal(results.offers[offerEventHashes[0]]
+              .operations[0].operation.record.id);
+            callback();
+          });
+        }],
+      }, err => {
+        assertNoError(err);
+        done();
+      });
+    });
+    it('returns NotFoundError when there are no matching records', done => {
+      async.auto({
+        concerts: callback => {
+          const eventTemplate = mockData.events.alpha;
+          const opTemplate = mockData.operations.alpha;
+          helpers.addEvent({
+            consensus: true, count: 5, eventTemplate, ledgerStorage, opTemplate
+          }, callback);
+        },
+        offers: ['concerts', (results, callback) => {
+          const eventTemplate = mockData.events.alpha;
+          const opTemplate = mockData.operations.gamma;
+          const eventHashes = Object.keys(results.concerts);
+          opTemplate.record.event = results.concerts[eventHashes[0]]
+            .operations[0].operation.record.id;
+          helpers.addEvent({
+            consensus: true, count: 1, eventTemplate, ledgerStorage, opTemplate
+          }, callback);
+        }],
+        query: ['offers', (results, callback) => {
+          // NOTE: querying for an unknown type
+          ledgerStorage.operations.query({
+            maxBlockHeight: 100,
+            query: {
+              type: 'UnknownType',
+            }
+          }, (err, result) => {
+            should.exist(err);
+            should.not.exist(result);
+            should.exist(err.name);
+            err.name.should.equal('NotFoundError');
+            callback();
+          });
+        }],
+      }, err => {
+        assertNoError(err);
+        done();
+      });
+    });
+  }); // end query API
 });
---
+++ test/mocha/mock.plugin.js
@@ -0,0 +1,96 @@
+/*!
+ * Copyright (c) 2017-2018 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+module.exports = {
+  type: 'service',
+  api: {
+    index: async ({createIndexes, collections}) => {
+      should.exist(createIndexes);
+      createIndexes.should.be.a('function');
+      should.exist(collections);
+      collections.should.an('object');
+      should.exist(collections.blockCollection);
+      collections.blockCollection.should.be.a('string');
+      should.exist(collections.eventCollection);
+      collections.eventCollection.should.be.a('string');
+      should.exist(collections.operationCollection);
+      collections.operationCollection.should.be.a('string');
+      await createIndexes([{
+        collection: collections.operationCollection,
+        fields: {'operation.record.type': 1},
+        options: {unique: false, background: false, name: 'fooIndex'}
+      }]);
+    },
+    storage: {
+      operations: {
+        // NOTE: do not use arrow functions here because this function is to be
+        // bound to the class instance
+        query: function({maxBlockHeight, query}, callback) {
+          const {
+            assert, dbHash, eventCollection, collection: operationCollection,
+            BedrockError
+          } = this;
+
+          assert.number(maxBlockHeight, 'maxBlockHeight');
+          assert.object(query, 'query');
+
+          const eventQuery = {
+            'event.type': 'WebLedgerOperationEvent',
+            'meta.blockHeight': {$lte: maxBlockHeight},
+            'meta.consensus': {$exists: true},
+          };
+          const operationMatch = {
+            $and: [
+              {$in: ['$meta.eventHash', '$$eventHashes']}
+            ]
+          };
+          Object.keys(query).forEach(k => {
+            if(Array.isArray(query[k])) {
+              operationMatch.$and.push(
+                {$in: [`$operation.record.${k}`, query[k]]});
+            } else if(k === 'id') {
+              operationMatch.$and.push(
+                {$eq: ['$recordId', dbHash(query[k])]});
+            } else {
+              operationMatch.$and.push(
+                {$eq: [`$operation.record.${k}`, query[k]]});
+            }
+          });
+          eventCollection.aggregate([
+            {$match: eventQuery},
+            {$project: {_id: 0, eventHash: 1}},
+            {$group: {
+              _id: null,
+              eventHashes: {$addToSet: '$eventHash'}
+            }},
+            {$lookup: {
+              from: operationCollection.s.name,
+              let: {eventHashes: '$eventHashes'},
+              pipeline: [
+                {$match: {$expr: operationMatch}},
+                {$project: {_id: 0, 'operation.record.id': 1}},
+                {$group: {_id: null, records: {
+                  $addToSet: '$operation.record.id'
+                }}},
+                {$project: {_id: 0}},
+              ],
+              as: 'records',
+            }},
+            {$project: {records: {$arrayElemAt: ['$records', 0]}}},
+            {$replaceRoot: {newRoot: '$records'}}
+          ]).toArray((err, result) => {
+            if(err && err.code === 40228) {
+              return callback(new BedrockError('Not Found.', 'NotFoundError'));
+            }
+            if(err) {
+              return callback(err);
+            }
+            callback(null, result[0]);
+          });
+        }
+      }
+    }
+  }
+};
